112ad3c74d8239177885fa103bf2792621c4dd05
alfandimelanaspt <70300678+alfandimelanaspt@users.noreply.github.com>
2020-09-19 15:03:18 +0700

    Add files via upload

---
+++ Data Types.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val firstWord = "SMK "
+    val middleWord = "Telkom "
+    val lastWord = "Purwokerto "
+    println(firstWord + middleWord + lastWord)
+
+
+    val firstword = "Luas "
+    val middleword = "persegi "
+    val lastword = "panjang "
+    print(firstword + middleword + lastword)
+
+    val valueA:Int = 12
+    val valueB = 10
+    print(valueA * valueB)
+}
---
+++ Hello Kotlin.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Fandi "
+    val hobby = "Membaca buku "
+
+    print("Nama saya ")
+    println(name)
+    print("Hobby saya ")
+    println(hobby)
+
+}
